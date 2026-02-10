0cf0f41aa803f466429cb890798089dce252f64b
Jonah Shirts <jshirts03@gmail.com>
2026-02-09 21:37:20 -0700

    Ok maybe it works now?

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    port: 5174,
     url: 'http://localhost:5174',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
